b9d730c7eb4dfccb212aba900cdb0e1ea636bceb
pukky060342 <48791874+pukky060342@users.noreply.github.com>
2019-04-04 14:55:20 +0700

    Delete viewing.sql

--- viewing.sql
+++
@@ -1,7 +0,0 @@
-drop table viewing;
-create table viewing
-    ( id int primary key auto_increment,
-    client_no varchar(4) not null,
-    property_no varchar(4) not null,
-    view_date varchar(10) not null,
-    comment text);
