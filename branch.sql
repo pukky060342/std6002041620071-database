c6c6687594b3815e7f67ebecb2a28718c73489be
pukky060342 <48791874+pukky060342@users.noreply.github.com>
2019-04-04 14:23:19 +0700

    Delete branch.sql

--- branch.sql
+++
@@ -1,7 +0,0 @@
-drop table branch;
-create table branch
-(id int primary key auto_increment,
-branch_no varchar(4) not null,
-street text,
-city varchar(200) not null,
-postcode varchar(8) not null);
