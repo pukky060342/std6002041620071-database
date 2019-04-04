79911a2191dfb61dbcd5eea77481786da54a796c
pukky060342 <48791874+pukky060342@users.noreply.github.com>
2019-04-04 14:24:01 +0700

    Delete registration.sql

--- registration.sql
+++
@@ -1,8 +0,0 @@
-drop table registration;
-create table registration
-    ( id int primary key auto_increment,
-    client_no varchar(4) not null,
-    branch_no varchar(4) not null,
-    staff_no varchar(4) not null,
-    date_joined varchar(10) not null);
-
